4bf88646cf33ff1e0ee1160f9b52035fe033f0ea
DrewMcD25 <drewmcd25@yahoo.com>
2021-10-23 11:25:25 -0500

    changes

---
+++ index.js
@@ -0,0 +1,52 @@
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import Login from './components/lilo';
+import Home from './components/home'
+import Shop from './components/Shop'
+import Cart from './components/cart'
+import Navbar from './components/navbar';
+import Register from './components/register'
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
+import './components/style/home.css'
+
+
+const App = () => {
+    const [isNewUser, setIsNewUser] = useState(true)
+    const [postList, setPostList] = useState([])
+    const renderAuthForm = (e) => {
+        if (isNewUser) {
+            return <Login toggleNewUser={setIsNewUser} />
+        } else {
+            return <Register toggleNewUser />
+        }
+
+    }
+
+    return (
+        <>
+            <Router>
+                <div id="navbar">
+                    <Navbar />
+                </div>
+                <main>
+                    <Switch>
+                        <div>
+                            <Route exact path="/login"><Login /></Route>
+                            <Route exact path="/shop"><Shop/></Route>
+                            <Route exact path="/cart"><Cart/></Route>
+                            <Route exact path="/register"><Register /></Route>
+                            <Route exact path="/"><Home /></Route>
+                        </div>
+                    </Switch>
+                </main>
+            </Router>
+
+
+
+        </>
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/components/shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Shop = () => {
+    return (
+        <div id ="profile">
+            <h1>SHOP</h1>
+        </div>
+    )
+}
+
+export default Shop
